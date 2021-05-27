84f15923d2c5b3a49f55f33f18f8a10d369fe5e3
WillieC92 <williecolvin92@gmail.com>
2021-05-27 15:02:11 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,99 @@
+let friends = ["Devan", "Josh", "David", "Jarvis"];
+
+for (let index = 0; index < 5; index++);
+
+let i = 99;
+while (i > 0) {
+  if (i === 1);
+
+  console.log(friends[1] + ":" + i + " " + " lines of code in the file,");
+
+  console.log(i + " " + "lines of code,");
+
+  console.log("Josh strikes one out,clears it all out,");
+
+  i--;
+
+  console.log(i + " " + " lines of code in the file.");
+}
+
+if (i === 1);
+
+{
+  console.log(
+    (i =
+      " 0 line of code in the file, 0 line of code. Josh strikes one out, clears it all out, no more lines of code in the file.")
+  );
+}
+
+let d = 99;
+while (d > 0) {
+  if (d === 1);
+
+  console.log(friends[0] + ":" + d + " " + " lines of code in the file,");
+
+  console.log(d + " " + "lines of code,");
+
+  console.log("Devan strikes one out,clears it all out,");
+
+  d--;
+
+  console.log(d + " " + " lines of code in the file.");
+}
+
+if (d === 1);
+
+{
+  console.log(
+    (d =
+      " 0 line of code in the file, 0 line of code. Devan strikes one out, clears it all out, no more lines of code in the file.")
+  );
+}
+
+let j = 99;
+while (j > 0) {
+  if (j === 1);
+
+  console.log(friends[2] + ":" + j + " " + " lines of code in the file,");
+
+  console.log(j + " " + "lines of code,");
+
+  console.log("David strikes one out,clears it all out,");
+
+  j--;
+
+  console.log(j + " " + " lines of code in the file.");
+}
+
+if (j === 1);
+
+{
+  console.log(
+    (j =
+      " 0 line of code in the file, 0 line of code. David strikes one out, clears it all out, no more lines of code in the file.")
+  );
+}
+
+let z = 99;
+while (z > 0) {
+  if (z === 1);
+
+  console.log(friends[3] + ":" + z + " " + " lines of code in the file,");
+
+  console.log(z + " " + "lines of code,");
+
+  console.log("Jarvis strikes one out,clears it all out,");
+
+  z--;
+
+  console.log(z + " " + " lines of code in the file.");
+}
+
+if (z === 1);
+
+{
+  console.log(
+    (z =
+      " 0 line of code in the file, 0 line of code. Jarvis strikes one out, clears it all out, no more lines of code in the file.")
+  );
+}
